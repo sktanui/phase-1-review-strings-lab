c09bddbe28001b29e924fc9f3eeb64fe140dc6f1
sktanui <shadrackkemei2019@gmail.com>
2024-09-23 12:08:28 +0300

    Complete second lab

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code in this file!
+const currentUser = "Grace Hopper"
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+let excitedWelcomeMessage = welcomeMessage.toUpperCase();
+let shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
+
+
+
